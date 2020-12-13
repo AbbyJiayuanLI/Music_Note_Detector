b46724ebf28d0bf881267faeb9a4c741cf002d38
TonyXN <56809878+TonyXN@users.noreply.github.com>
2020-12-13 18:42:28 +0800

    Add files via upload
    now we can play the sound! hahaha

--- Code/Task3_freq_HPS.m
+++ Code/Task3_freq_HPS.m
@@ -1,8 +1,8 @@
-%% 
+%% Load Music
 clc
 clear
 
-tic
+
 [music, fs] = audioread('fur_elise_single_70.mp3');
 % [music, fs] = audioread('fur_elise_single_40.mp3');
 % [music, fs] = audioread('octave_c3_c4.mp3');
@@ -14,18 +14,26 @@ tic
 music = music(:,1);   % one channel
 % music = music(1*fs:length(music));
 % music = music(1*fs:3*fs);
+
+%% HPS
+
 blockSize = 6000;
 fftSize = fs/2;
 stepSize = 400;
+threshold = 0.0001;
+decay = 0.9;
+
+plotHPSProcess = 0;
+
 N = length(music);
 numRound = round((N-blockSize)/stepSize);
 noteTrack = zeros(numRound, 1);
 position = 1; 
 i = 1;
-threshold = 0.0001;
-decay = 0.9;
+
 freqSet = zeros(numRound, 1);
 % energy = [];
+tic
 while (position+blockSize < N)
     win = hanning(blockSize);
     frame = music(position:position+blockSize-1).*win;
@@ -71,28 +79,17 @@ while (position+blockSize < N)
         len = length(hps3);
         totalHPS = hps1(1:len).*hps2(1:len).*hps3;
 
-    %     hps4 = downsample(FFT,4);
-    %     len = length(hps4);
-    %     totalHPS = hps1(1:len).*hps2(1:len).*hps3(1:len).*hps4;
-        plotProcess = 0;
-        if (plotProcess == 1)
-            figure(2);
-            lim = 1000;
-            subplot(5,1,1);
-            plot(hps1);
-            xlim([0, lim]);
-            subplot(5,1,2);
-            plot(hps2);
-            xlim([0, lim]);
-            subplot(5,1,3);
-            plot(hps3);
-            xlim([0, lim]);
-            subplot(5,1,4);
-            plot(hps4);
-            xlim([0, lim]);
-            subplot(5,1,5);
-            plot(totalHPS);
-            xlim([0, lim]);
+%         hps4 = downsample(FFT,4);
+%         len = length(hps4);
+%         totalHPS = hps1(1:len).*hps2(1:len).*hps3(1:len).*hps4;
+        
+        if (plotHPSProcess == 1)
+            figure(2);lim = 1000;
+            subplot(5,1,1);plot(hps1);xlim([0, lim]);
+            subplot(5,1,2);plot(hps2);xlim([0, lim]);
+            subplot(5,1,3);plot(hps3);xlim([0, lim]);
+            subplot(5,1,4);plot(hps4);xlim([0, lim]);
+            subplot(5,1,5);plot(totalHPS);xlim([0, lim]);
         end
 
         fmin = 50;
@@ -119,7 +116,6 @@ while (position+blockSize < N)
     %         if (idx(2) < 0.55*idx(1) && idx(2) > 0.45*idx(1) && peaks(2) >= 0.5*peaks(1) && anti == 1)
     %             idx(1) = idx(2);
     %         end
-
     %         freq = (idx(1)-1)*fs/fftSize;
             freq = (idx(1)-1)*fs/fftSize;
             freqSet(i) = freq;
@@ -134,9 +130,9 @@ end
 % noteTrack = movmax(noteTrack, 5);
 % noteTrack = movmin(noteTrack, 10);
 
-% noteTrack = medfilt1(noteTrack, 5);
-% noteTrack = movmax(noteTrack, 5);
-% noteTrack = movmax(noteTrack, 5);
+noteTrack = medfilt1(noteTrack, 5);
+noteTrack = movmax(noteTrack, 5);
+noteTrack = movmax(noteTrack, 5);
 
 % noteTrack = medfilt1(noteTrack, 10);
 
@@ -144,23 +140,11 @@ toc
 %% Plot
 
 figure(3);
-% for i=2:1:length(noteTrack)
-%     if (abs(noteTrack(i) - noteTrack(i-1)) > 15)
-%         noteTrack(i) = noteTrack(i-1);
-%     end
-% end
-
 t = 0:N/fs/(length(noteTrack)-1):N/fs;
 plot(t, noteTrack)
 xlabel('Time [sec]')
 
 %% Play Sound
-% 
-% freqTrack = zeros(numRound, 1);
-% for i=1:1:length(noteTrack)
-%     note = (12*log2(freq/440));
-%     freqTrack(i) = 440*2^(noteTrack(i)/12);
-% end
-% 
-% music_reconstruct = resample(freqTrack, round(fs/(length(noteTrack)/(N/fs))), 1);
-% sound(music_reconstruct,fs);
+% music_reconstruct = note2music(noteTrack, fs, N);
+% sound(music_reconstruct, fs);
+
